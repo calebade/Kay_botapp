a9c4133e97401fa9b1be86b46a91ef54b54f10ba
calebade <52384493+calebade@users.noreply.github.com>
2024-01-27 00:55:56 +0100

    Add files via upload

---
+++ Kay_bot.py
@@ -0,0 +1,26 @@
+# app.py
+import streamlit as st
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    sentiment_score = blob.sentiment.polarity
+
+    if sentiment_score > 0:
+        return "Positive"
+    elif sentiment_score < 0:
+        return "Negative"
+    else:
+        return "Neutral"
+
+def main():
+    st.title("Sentiment Analysis App")
+    user_input = st.text_area("Enter text:")
+    
+    if st.button("Analyze"):
+        if user_input:
+            sentiment = analyze_sentiment(user_input)
+            st.write(f"Sentiment: {sentiment}")
+
+if __name__ == "__main__":
+    main()
